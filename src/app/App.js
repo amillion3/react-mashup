73b94c642b042c47effd54153a98791376c7e91c
Andy <amillion3@gmail.com>
2018-06-27 20:16:14 -0500

    added Animals component

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Animals from '../components/Animals/Animals';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className='btn btn-info'>Allo!</button>
+        <div className='col-xs-12 col-md-6'>
+          <Animals />
+        </div>
+        <div className='col-xs-12 col-md-6'>
+
+        </div>
       </div>
     );
   }
