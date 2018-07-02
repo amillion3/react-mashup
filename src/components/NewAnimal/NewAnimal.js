9fce537e94bca1b2ceb564647dfaf6b695047e9a
Andy <amillion3@gmail.com>
2018-07-01 21:47:08 -0500

    WIP

--- src/components/NewAnimal/NewAnimal.js
+++ src/components/NewAnimal/NewAnimal.js
@@ -4,27 +4,55 @@ import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 import './NewAnimal.css';
 
 class NewAnimal extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {newName: ''};
+
+    this.handleChange = this.handleChange().bind(this);
+    this.handleSubmit = this.handleSubmit().bind(this);
+  }
+
+  handleChange (e) {
+    this.setState({newName: e.target.value});
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    console.error('submitted: ', this.state.value);
+  }
+
+  newAnimalClick = e => {
+    e.preventDefault();
+    // const {} = this.props;
+    console.error(e);
+    console.error(this);
+    // this.newName.setState(e.target);
+  }
+
   render () {
     return (
       <div className='NewAnimal'>
         <h2>Add New Animal</h2>
         <form>
-          <FormGroup controlId="formBasicText">
+          <FormGroup controlId="formBasicText" onSubmit={this.handleSubmit}>
             <ControlLabel>Name:</ControlLabel>
             <FormControl type="text" placeholder="Enter text"
-              // value={this.state.value}
+              value={this.state.newName}
               // onChange={this.handleChange}
             />
             <ControlLabel>Image URL:</ControlLabel>
             <FormControl type="text" placeholder="Enter URL"
-              // value={this.state.value}
+              value={this.state.newUrl}
               // onChange={this.handleChange}
             />
             <FormGroup controlId="formControlsTextarea">
               <ControlLabel>Description</ControlLabel>
-              <FormControl componentClass="textarea" placeholder="Please describe the animal." />
+              <FormControl
+                componentClass="textarea"
+                placeholder="Please describe the animal."
+                value={this.state.newDesc} />
             </FormGroup>
-            <Button type='submit'>Submit</Button>
+            <Button type='submit' onClick={this.newAnimalClick}>Submit</Button>
 
           </FormGroup>
         </form>
