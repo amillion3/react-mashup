8641451e649d240a0645925aea4c12a619c2068d
Andy <amillion3@gmail.com>
2018-07-04 14:46:42 -0500

    WIP, making POST request

--- src/app/App.js
+++ src/app/App.js
@@ -5,10 +5,24 @@ import NewAnimal from '../components/NewAnimal/NewAnimal';
 import connection from '../firebaseRequests/connection';
 import getAnimalsRequest from '../firebaseRequests/animalsRequest';
 import './App.css';
+import animalsRequest from '../firebaseRequests/animalsRequest';
 
 class App extends React.Component {
   state = {
     animals: [],
+  }
+
+  formSubmitEvent = newAnimal => {
+    animalsRequest.newAnimalRequest(newAnimal)
+      .then(() => {
+        animalsRequest.getAnimalsRequest()
+          .then(animals => {
+            this.setState({animals});
+          });
+      })
+      .catch(err => {
+        console.error('error with animals POST', err);
+      });
   }
 
   componentDidMount () {
